feba6c0817eabfc4cd41025fe05e339dab827833
guoguihong <guoguihong@qq.com>
2021-09-08 15:57:38 +0800

    前端api区分开发环境和生产环境;后端添加静态文件托管

--- server/index.js
+++ server/index.js
@@ -2,17 +2,52 @@ const http = require('http')
 const url = require('url')
 const fs = require('fs')
 const path = require('path')
+const { promisify } = require('util')
 
-const server = http.createServer((req, res) => {
+const fsExistsAsync = promisify(fs.exists)
+
+const server = http.createServer(async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Content-Type', 'application/json;charset=utf8')
-  const pathName = url.parse(req.url).pathname
+  let pathName = url.parse(req.url).pathname
   const apiFilePath = path.join(__dirname, `api${pathName}.js`)
-  fs.exists(apiFilePath, isExists => {
-    isExists && require(apiFilePath)(req, res)
-  })
+  let isExists = await fsExistsAsync(apiFilePath)
+  if (isExists) {
+    require(apiFilePath)(req, res)
+    return
+  }
+  if (pathName === '/') {
+    pathName = '/index.html'
+  }
+  pathName = path.join(__dirname, './public/dist' + pathName)
+  isExists = await fsExistsAsync(pathName)
+  if (isExists) {
+    fs.readFile(pathName, 'utf8', function(err, data) {
+      if (err) {
+        console.error(err)
+        res.statusCode = 404
+        res.end()
+        return
+      }
+      const ext = pathName.slice(pathName.lastIndexOf('.') + 1)
+      switch (ext) {
+        case 'html':
+          res.setHeader('Content-Type', 'text/html;charset=uft-8')
+          break
+        case 'css':
+          res.setHeader('Content-Type', 'text/css')
+          break
+        case 'js':
+          res.setHeader('Content-Type', 'text/javascript')
+          break
+      }
+      res.write(data.toString())
+      res.end()
+    })
+    return
+  }
 })
 
-server.listen(3000, () => {
-  console.log('running');
+server.listen(3003, () => {
+  console.log('running')
 })
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,12 +1,15 @@
 import axios from 'axios'
 
+const baseURL =
+  process.env.NODE_ENV === 'development'
+    ? '//localhost:3003'
+    : '//monority.ggh0807.cn'
 const service = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL,
   timeout: 5000,
   withCredentials: false
 })
 
-service.interceptors.response.use(response => response.data)
+service.interceptors.response.use((response) => response.data)
 
 export default service
-
